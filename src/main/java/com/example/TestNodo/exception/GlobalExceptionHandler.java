6013fff2c3c22cba3e2ab8ab15a6712a6a7ab770
ADMIN <daiphung79@gmail.com>
2025-06-09 16:10:06 +0700

    first commit

--- src/main/java/com/example/TestNodo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/TestNodo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.TestNodo.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
